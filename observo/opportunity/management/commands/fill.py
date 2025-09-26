2ac1bf96dfe8a71735cc06d90724922b0e69a67f
Dawid Hanrahan <dawidhanrahan@gmail.com>
2025-09-26 09:15:57 +0200

    Add recalculation parameter to fill command

--- observo/opportunity/management/commands/fill.py
+++ observo/opportunity/management/commands/fill.py
@@ -12,17 +12,21 @@ class Command(BaseCommand):
     help = "Populate field in Opportunity model, e.g. applications and success rate"
 
     def add_arguments(self, parser):
+        parser.add_argument("--recalculate", action="store_true", help="Force recalculation for all Opportunities.")
         parser.add_argument("--no-progress", action="store_true", help="Disable tqdm progress bar.")
 
     @transaction.atomic
     def handle(self, *args, **options):
-        queryset = Opportunity.objects.filter(Q(applications__isnull=True) | Q(success_rate__isnull=True))
+        if options["recalculate"]:
+            queryset = Opportunity.objects.all()
+        else:
+            queryset = Opportunity.objects.filter(Q(applications__isnull=True) | Q(success_rate__isnull=True))
         self.stdout.write(f"{queryset.count()} Opportunities will be populated with `applications` and `success_rate`")
 
         iterator = queryset if options["no_progress"] else tqdm(queryset, total=queryset.count(), unit="objects")
         for opportunity in iterator:
             applications = random.randint(a=10, b=25)
-            success_rate = random.randint(a=3, b=5) / applications
+            success_rate = random.randint(a=3, b=5) / applications * 100  # Value as percentage, e.g. 20
             opportunity.applications = applications
             opportunity.success_rate = success_rate
             opportunity.save()
