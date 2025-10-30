41003592faf52a43090938ab46a6821a477a1aa4
Dawid Hanrahan <dawidhanrahan@gmail.com>
2025-10-30 17:13:47 +0100

    Change description of workflow rerun action

--- observo/search/actions/rerun_generation.py
+++ observo/search/actions/rerun_generation.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from search.tasks import prepare_outline
 
 
-@admin.action(description="Rerun Preparation for Selected Outlines")
+@admin.action(description="Rerun Workflow for Selected Outlines")
 def rerun_generation(model_admin, request, queryset) -> None:
     for notification in queryset:
         prepare_outline.delay(notification.pk)
